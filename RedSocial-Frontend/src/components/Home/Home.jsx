53fdd5844721f7700abb7d6c28ad438d338a67a7
arnoldm94 <arnoldmelet18@gmail.com>
2025-05-03 00:54:33 +0200

    2.0

--- RedSocial-Frontend/src/components/Home/Home.jsx
+++ RedSocial-Frontend/src/components/Home/Home.jsx
@@ -1,14 +1,15 @@
 import React from "react";
 import "./Home.styles.scss";
 import Posts from "../Posts/Posts";
+import Card from "../Posts/Card";
 
 const Home = () => {
   return (
     <>
       <h1>Red Social - Arnold</h1>
-
       <div className="title">
         <Posts />
+        <Card />
       </div>
     </>
   );
