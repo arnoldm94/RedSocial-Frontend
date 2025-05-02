ac907f9e9d0720f33b49b91ec2a9a001123e640a
arnoldm94 <arnoldmelet18@gmail.com>
2025-05-03 00:07:07 +0200

    version 2.0

--- RedSocial-Frontend/src/components/Home/Home.jsx
+++ RedSocial-Frontend/src/components/Home/Home.jsx
@@ -10,38 +10,6 @@ const Home = () => {
       <div className="title">
         <Posts />
       </div>
-
-      <div id="carouselExample" className="carousel slide">
-        <div className="carousel-inner">
-          <div className="carousel-item active">
-            <img src="/img/holy.png" className="d-block w-100" alt="..." />
-          </div>
-          <div className="carousel-item">
-            <img src="/img/holy.png" className="d-block w-100" alt="..." />
-          </div>
-          <div className="carousel-item">
-            <img src="/img/holy.png" className="d-block w-100" alt="..." />
-          </div>
-        </div>
-        <button
-          className="carousel-control-prev"
-          type="button"
-          data-bs-target="#carouselExample"
-          data-bs-slide="prev"
-        >
-          <span className="carousel-control-prev-icon" aria-hidden="true"></span>
-          <span className="visually-hidden">Previous</span>
-        </button>
-        <button
-          className="carousel-control-next"
-          type="button"
-          data-bs-target="#carouselExample"
-          data-bs-slide="next"
-        >
-          <span className="carousel-control-next-icon" aria-hidden="true"></span>
-          <span className="visually-hidden">Next</span>
-        </button>
-      </div>
     </>
   );
 };
